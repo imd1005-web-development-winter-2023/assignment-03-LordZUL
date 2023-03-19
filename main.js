32f4a7cb30ea6273a4c9cf34eb7cfa7190d8d2e6
Alanna You <alannayou.z@gmail.com>
2023-03-19 02:44:21 -0400

    Finished todo List section of coding

--- main.js
+++ main.js
@@ -18,21 +18,51 @@ const clearButton = document.querySelector(".clear-button");
 //console.log(inputField,todoList,todoListForm,pendingNum,clearButton);
 
 //register that enter key will enter data
+function allTasks() {
+  let tasks = document.querySelectorAll(".pending");
+  //console.log(tasks);
+  pendingNum.textContent = tasks.length === 0? "no" : tasks.length;
+
+  let allLists = document.querySelectorAll(".list");
+  if(allLists.length > 0){
+    todoList.getElementsByClassName.marginTop = "20px";
+    clearButton.style.pointerEvents = "auto";
+    return;
+  }
+  todoLists.style.marginTop = "0px";
+  clearButton.style.pointerEvents = "none";
+}
 inputField.addEventListener("keyup",(e) =>{
   let inputVal = inputField.value.trim(); //removes white space 
-
-  if (e.key === "Enter" && inputVal.length > 0) {
-    let liTag = `<li class="list">
+  if ( e.key === "Enter" && inputVal.length > 0) {
+    let liTag = `<li class="list pending" onclick = "handleStatus(this)">
           <input type="checkbox">
           <span class="task">${inputVal}</span>
-          <i class="ri-delete-bin-5-line"></i>
+          <i class="ri-delete-bin-5-line" onclick = "deleteTask(this)"></i>
         </li>`;
    todoList.insertAdjacentHTML("beforeend", liTag);
-    console.log("Valid");
+   todoListForm.reset();
+   allTasks();
 
   }
 })
 
+function handleStatus(e){
+  const checkbox = e.querySelector("input");
+  checkbox.checked = checkbox.checked ? false : true;
+  e.classList.toggle("pending");
+  allTasks();
+}
+
+function deleteTask(e){
+  e.parentElement.remove();
+  allTasks();
+}
+function clearList(e){
+  todoList.innerHTML = "";
+  allTasks();
+}
+clearButton.addEventListener("click", clearList);
 
 // Variables
 
